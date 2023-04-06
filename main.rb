bdac3d70114cc789a22a9ab7b1f1abcfbd272346
Talha-Hanif5666 <talhahanif5666@gmail.com>
2023-04-06 13:35:33 +0500

    Crreate MyList class

--- main.rb
+++ main.rb
@@ -21,4 +21,28 @@ module MyEnumerable
     result
   end
 end
+
+class MyList
+  include MyEnumerable
+
+  def initialize(*data)
+    @list = Array.new(data)
+  end
+
+  def each
+    @list.each do |item|
+      yield item
+    end
+  end
+end
+
+list = MyList.new(1, 2, 3, 4)
+
+p list.all? {|e| e < 5}
+p list.all? {|e| e > 5}
+
+p list.any? {|e| e == 2}
+p list.any? {|e| e == 5}
+
+p list.filter {|e| e.even?}
   
